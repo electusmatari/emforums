864c42920be629727bb76b127eaa4e410eb0252d
Jorgen Schaefer <forcer@forcix.cx>
2013-09-28 13:50:59 +0200

    Added some language files

---
+++ Upload/inc/languages/english/reCAPTCHA_plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+	// MyBB reCAPTCHA plugin U.S. English language file.
+	// (C) 2010 CubicleSoft.  All Rights Reserved.
+
+	$l["reCAPTCHA_verification_note"] = "Please enter the text contained within the image into the text box below it.  This is necessary to prevent automated signups.";
+
+	$l["userdata_reCAPTCHA_plugin_Error_NoChallengeResponse"] = "CAPTCHA challenge-response is missing.";
+	$l["userdata_reCAPTCHA_plugin_Error_Incorrect"] = "Incorrect CAPTCHA entered.  Try again.  (Code:  {1})";
+?>
