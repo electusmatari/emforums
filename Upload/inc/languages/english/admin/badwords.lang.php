a2e05d01bec7afc5a6a62299108537d0f4a3df36
Jorgen Schaefer <forcer@forcix.cx>
2013-09-28 13:50:59 +0200

    Added some language files

---
+++ Upload/inc/languages/english/admin/badwords.lang.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MyBB 1.2 English Language Pack
+ * Copyright © 2006 MyBB Group, All Rights Reserved
+ * 
+ * $Id: badwords.lang.php 1398 2006-04-27 07:42:09Z chris $
+ */
+
+$l['nav_badwords'] = "Bad Word Filters";
+$l['nav_add_badword'] = "Add Bad Word Filter";
+$l['nav_edit_badword'] = "Edit Bad Word Filter";
+$l['nav_delete_badword'] = "Delete Bad Word Filter";
+
+$l['badword_added'] = "The bad word filter has successfully been added. You will now be taken to the bad word manager.";
+$l['badword_edited'] = "The bad word filter has successfully been edited. You will now be taken to the bad word manager.";
+$l['modify_badword'] = "Modify Bad Word Filter";
+$l['badword_title'] = "Bad Word";
+$l['badword_label'] = "Bad Word";
+$l['replacement_title'] = "Replaced With";
+$l['replacement_label'] = "Replaced With";
+$l['update_badword'] = "Update Filter";
+$l['add_badword'] = "Add Bad Word Filter";
+$l['insert_badword'] = "Add Filter";
+$l['badwords'] = "Bad Word Filters";
+$l['edit'] = "Edit";
+$l['delete'] = "Delete";
+$l['add_badword_filter'] = "New Bad Word Filter";
+$l['options'] = "Options";
+$l['badword_deleted'] = "The bad word filter has been deleted. You will now be taken to the bad word management page.";
+$l['no_badwords'] = "There are currently no bad word filters. To create a bad word filter, click the button above.";
+?>
